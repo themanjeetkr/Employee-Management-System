e953d4f5ff52317fbef3db3b02255f37538ce366
themanjeetkr <themanjeetkr@gmail.com>
2025-09-07 00:21:38 +0530

    login page completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+// import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './Components/Auth/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <div>
+      <Login/>
+     </div>
     </>
   )
 }
