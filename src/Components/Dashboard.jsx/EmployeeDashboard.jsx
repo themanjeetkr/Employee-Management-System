1c22bda846782f741fc81707f2397c378b0af0ac
themanjeetkr <themanjeetkr@gmail.com>
2025-09-07 23:07:08 +0530

    add tasl;list

--- src/Components/Dashboard.jsx/EmployeeDashboard.jsx
+++ src/Components/Dashboard.jsx/EmployeeDashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../Other/Header'
 import TaskListNumber from '../Other/TaskListNumber'
+import TaskList from '../../Components/TaskList/TaskList'
 
 const EmployeeDashboard = () => {
   return (
@@ -8,6 +9,7 @@ const EmployeeDashboard = () => {
       
     <Header/>
     <TaskListNumber/>
+    <TaskList/>
     </div>
   )
 }
---
+++ src/Components/TaskList/TaskList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div id='tasklist' className='h-[55%] flex  w-full py-5  overflow-x-auto items-center justify-start mt-10 flex-nowrap gap-10'> 
+    <div className='h-full  flex-shrink-0 w-[300px] bg-yellow-400 rounded-xl'></div>
+  
+         
+    </div>
+  )
+}
+
+export default TaskList
