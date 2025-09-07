f889769ffe1e7384ec8bf0387c7ce2b5eedd428d
themanjeetkr <themanjeetkr@gmail.com>
2025-09-07 22:31:10 +0530

    added task list numbers

--- src/Components/Dashboard.jsx/EmployeeDashboard.jsx
+++ src/Components/Dashboard.jsx/EmployeeDashboard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../Other/Header'
+import TaskListNumber from '../Other/TaskListNumber'
 
 const EmployeeDashboard = () => {
   return (
     <div className='p-10 bg-[#1c1c1c] text-white h-screen'>
       
     <Header/>
+    <TaskListNumber/>
     </div>
   )
 }
